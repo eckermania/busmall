6d20fab189c8f314f67ffdd2e181a5ac088fbfae
eckermania <erineckerman@gmail.com>
2018-08-20 21:11:10 -0700

    completed randomization

--- app.js
+++ app.js
@@ -1,1 +1,94 @@
 'use strict';
+
+var imageGroup = document.getElementById('image-container');
+
+var imageElOne = document.getElementById('image-one');
+
+var imageElTwo = document.getElementById('image-two');
+
+var imageElThree = document.getElementById('image-three');
+
+var allProducts = [];
+
+var currentProducts = [];
+
+var priorProducts = [];
+
+var votes = 0;
+
+function Product(name) {
+  this.name = name;
+  this.timesShown = 0;
+  this.path = `img/${name}.jpg`;
+  allProducts.push(this);
+}
+
+var allProductNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+allProductNames.forEach(function(productName) {
+  new Product(productName);
+});
+
+function showProducts() {
+
+  function showRandomProductOne() {
+    var randoOne = Math.floor(allProducts.length * Math.random());
+    while (priorProducts.includes(randoOne)) {
+      randoOne = Math.floor(allProducts.length * Math.random());
+    }
+    imageElOne.src = allProducts[randoOne].path;
+    imageElOne.title = allProducts[randoOne].name;
+    allProducts[randoOne].timesShown++;
+    currentProducts.push(randoOne);
+  }
+
+  showRandomProductOne();
+
+  function showRandomProductTwo() {
+
+    var randoTwo = Math.floor(allProducts.length * Math.random());
+    while (currentProducts.includes(randoTwo) || priorProducts.includes(randoTwo)){
+      randoTwo = Math.floor(allProducts.length * Math.random());
+    }
+    imageElTwo.src = allProducts[randoTwo].path;
+    imageElTwo.title = allProducts[randoTwo].name;
+    allProducts[randoTwo].timesShown++;
+    currentProducts.push(randoTwo);
+  }
+
+  showRandomProductTwo();
+
+  function showRandomProductThree() {
+    var randoThree = Math.floor(allProducts.length * Math.random());
+    while (currentProducts.includes(randoThree) || priorProducts.includes(randoThree)){
+      randoThree = Math.floor(allProducts.length * Math.random());
+    }
+    imageElThree.src = allProducts[randoThree].path;
+    imageElThree.title = allProducts[randoThree].name;
+    allProducts[randoThree].timesShown++;
+    currentProducts.push(randoThree);
+  }
+
+  showRandomProductThree();
+
+  priorProducts = [];
+  priorProducts = currentProducts;
+  currentProducts = [];
+}
+
+showProducts();
+
+
+
+imageGroup.addEventListener('click', function(event) {
+  if (votes < 2) {
+  // TODO: CHANGE VOTES ABOVE TO 25
+    showProducts(event);
+    votes ++;
+
+  } else {
+    console.log(document.getElementById('start-layout'));
+    document.getElementById('start-layout').classList.add('hidden');
+    document.getElementById('end-layout').classList.remove('hidden');
+  }
+});
